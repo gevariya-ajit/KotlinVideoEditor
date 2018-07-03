82bb3bc2f7085b2622b7dc6528c5b3f8085692aa
Ajit Gevariya <gevariyaajit@gmail.com>
2018-07-03 23:36:03 +0530

    package update

--- android/src/main/java/com/platforms/editor/MainActivity.kt
+++ android/src/main/java/com/platforms/editor/MainActivity.kt
@@ -2,8 +2,6 @@ package org.konan.calculator
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.EditText
-import android.widget.TextView
 import com.platforms.editor.R
 
 class MainActivity : AppCompatActivity() {
