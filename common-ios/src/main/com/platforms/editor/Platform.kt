c8118a1a0b7961c39c29e05ba48e8a58850d245b
Ajit Gevariya <gevariyaajit@gmail.com>
2018-07-05 00:31:28 +0530

    ios build fix

--- common-ios/src/main/com/platforms/editor/Platform.kt
+++ common-ios/src/main/com/platforms/editor/Platform.kt
@@ -4,6 +4,6 @@ actual open class Platform actual constructor(
     private val url: String
 ) {
     actual fun get():String {
-        return "$url Kotlin Native"
+        return "$url Kotlin Native for iOS"
     }
 }
